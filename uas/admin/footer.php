e2a1980fe903e301c2fe2f03370852d35065a55c
SilvyMilda <100079356+SilvyMilda@users.noreply.github.com>
2022-06-29 16:12:24 +0700

    Delete uas directory

--- uas/admin/footer.php
+++
@@ -1,5 +0,0 @@
-<footer style="background-color: gray;padding: 1px;" class="print">
-	<h5 class="text-center">Copyright&copy; Silvy Milda Puspita dan Roro Ayu Pangestuti</h5>
-</footer>
-</body>
-</html>
